d2ea619de04fdffbcdd9fc9cf4c15537e32b9737
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-07 00:54:24 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-for ch in range(97, 123):
-    print(chr(ch), end="")
-=======
 #!/usr/bin/python3
 for ch in range(97, 123):
     print("{:s}".format(chr(ch)), end="")
->>>>>>> 8837138ea9cc3b458715e46ae0f7e477729d9075
