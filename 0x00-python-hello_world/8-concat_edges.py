ca38971dfee334a68db03063575d78473df57cc9
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-07 14:18:28 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-word_first_3 = str[39:67]
-word_last_2 = str[:6]
-middle_word = str[114:119]
-print("{:s}{:s}{:s}".format(word_first_3, middle_word, word_last_2))
+str = str[39:67] + str[114:119] + str[:6]
+print(str)
