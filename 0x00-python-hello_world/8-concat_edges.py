914098748b9a90c9b8c42584675dbb8a2571686f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-07 14:09:11 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[114:119] + str[:6]
-print(str)
+word_first_3 = str.split()[0][:3]
+word_last_2 = str.split()[-1][-2:]
+middle_word = " ".join(str.split()[1:-1])
+print(f"{word_first_3}{middle_word}{word_last_2}")
