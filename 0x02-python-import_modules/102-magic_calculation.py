6d9c627262f42ac45a52a750be71b30bc53af119
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-09 01:36:19 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-    add = magic_calculation_102.add
-    sub = magic_calculation_102.sub
-    
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
