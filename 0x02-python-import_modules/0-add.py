a4b2f6c3e262caac3a221ee399ca57a55ab1da05
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-08 21:35:24 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-
-    from add_0 import add
-
     result = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
