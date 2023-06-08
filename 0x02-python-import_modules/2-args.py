3bb4cb0c75adf6aa03abc2db62f13b9c5c0a4ae9
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-08 21:47:47 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
 
-if num_arguments == 0:
-    print("0 arguments.")
-else:
-    print(
-        "{} argument{}:".format(
-            num_arguments, "s" if num_arguments != 1 else ""
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print(
+            "{} argument{}:".format(
+                num_arguments, "s" if num_arguments != 1 else ""
+            )
         )
-    )
-    for i, arg in enumerate(arguments, start=1):
-        print("{}: {}".format(i, arg))
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(i, arg))
