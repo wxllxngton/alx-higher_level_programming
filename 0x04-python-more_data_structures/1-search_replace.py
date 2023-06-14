3404b67e7b1c0b3af0994bcaf69d5bedd6c28d1f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-14 13:52:24 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_matrix = []
-    for i in range(len(my_list)):
-        new_matrix.append(my_list[i]) if i != search else new_matrix.append(
-            replace
-        )
-    return new_matrix
+    new_list = [replace if item == search else item for item in my_list]
+    return new_list
