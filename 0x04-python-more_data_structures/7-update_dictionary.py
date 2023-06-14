f5cf168d10fa8ed62a2e5e9ea78c4366fef44afb
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-14 13:50:51 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
     return a_dictionary
