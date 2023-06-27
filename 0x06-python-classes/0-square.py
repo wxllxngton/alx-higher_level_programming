51154deececb438acac05ced5d778762de79915a
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-28 01:06:48 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Square class represents a square shape.
+    """
+
     def __init__(self):
+        """
+        Initializes a Square instance.
+        """
         pass
