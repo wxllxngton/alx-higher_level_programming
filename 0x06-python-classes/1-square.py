04d5fb9e6a222440d187e50d92e17a37f2a4bad1
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-28 01:06:10 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Square class represents a square shape.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square. Must be an integer.
+
+        Raises:
+            TypeError: If size is not an integer.
+        """
         self.__size = size
+
