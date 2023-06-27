b89b2074c3c39f51e70c800453c9a7a0f72b31d7
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-06-28 01:17:26 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+
+"""
+Module 0 - Square class
+"""
+
 class Square:
     """
-    Square class represents a square shape.
+    Represents a square shape.
     """
 
     def __init__(self):
