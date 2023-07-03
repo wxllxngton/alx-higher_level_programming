dcaa1eb8770117f175af822e779332ef9e86dfef
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-03 13:11:41 +0300

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+"""
+0-rectangle module
+
+This module defines the Rectangle class.
+"""
+
 class Rectangle:
+    """
+    A class representing a rectangle.
+    """
+
     def __init__(self):
+        """
+        Initializes a new instance of the Rectangle class.
+        """
         pass
