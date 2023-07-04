45ffbe5458f18b2f6b855cfdcfb2199682e9493e
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-04 22:01:04 +0300

    Add files via upload

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+101-locked_class
+"""
+
+
+class LockedClass:
+    """A class that prevents dynamic creation of instance attributes, except for 'first_name'."""
+
+    # Limit the allowed instance attributes to 'first_name' using __slots__
+    __slots__ = ["first_name"]
+
+    def __setattr__(self, name, value):
+        """
+        Override the attribute assignment behavior.
+
+        Only allows assignment of 'first_name' instance attribute.
+        Raises an AttributeError for any other attribute assignment.
+        """
+        if name != "first_name":
+            raise AttributeError(
+                "'LockedClass' object does not support attribute assignment"
+            )
+        self.__dict__[name] = value
