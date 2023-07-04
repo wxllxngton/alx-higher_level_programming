f12c57e56b54e6213214b6adfed5c3c2c2f37f89
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-04 22:04:06 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 """
-101-locked_class
+LockedClass Module
 """
-
 
 class LockedClass:
     """A class that prevents dynamic creation of instance attributes, except for 'first_name'."""
 
     # Limit the allowed instance attributes to 'first_name' using __slots__
-    __slots__ = ["first_name"]
+    __slots__ = ['first_name']
 
     def __setattr__(self, name, value):
         """
@@ -18,8 +17,6 @@ class LockedClass:
         Only allows assignment of 'first_name' instance attribute.
         Raises an AttributeError for any other attribute assignment.
         """
-        if name != "first_name":
-            raise AttributeError(
-                "'LockedClass' object does not support attribute assignment"
-            )
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object does not support attribute assignment")
         self.__dict__[name] = value
