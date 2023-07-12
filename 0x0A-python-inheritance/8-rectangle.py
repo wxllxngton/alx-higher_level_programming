d44480511f04047696465ee69c9baba63a3e43af
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-12 10:38:44 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,10 +6,13 @@
 
 
 class Rectangle(BaseGeometry):
+    """
+    A class that represents a rectangle and inherits from BaseGeometry.
+    """
     def __init__(self, width, height):
         """
-        Initializes a Rectangle instance with private attributes W and H.
-        Validates the W and H as positive integers using integer_validator.
+        Initializes a Rectangle instance with private attributes width and height.
+        Validates the width and height as positive integers using integer_validator.
         """
         self.__width = 0
         self.__height = 0
