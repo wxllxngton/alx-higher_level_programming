d71fb8728785de7c8b6d6cf33e6b12e0cbd583a8
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-12 10:35:25 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,6 +6,9 @@
 
 
 class BaseGeometry:
+    """
+    Base class for geometry-related operations.
+    """
     def area(self):
         """
         Raises an Exception with the message 'area() is not implemented'.
@@ -21,35 +24,3 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with private attributes W and H.
-        Validates the W and H as positive integers using integer_validator.
-        """
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        Calculates and returns the area of the Rectangle.
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle's dimensions.
-        """
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-
-r = Rectangle(3, 5)
-
-print(r)
-print(r.area())
