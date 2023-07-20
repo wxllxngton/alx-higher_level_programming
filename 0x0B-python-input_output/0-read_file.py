668bedfac6da86b78b89b19fe27f66af7290e3dc
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-20 15:03:30 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+"""
+This is the 'write_file' module.
 
+It contains a function that writes a string to a text file (UTF8) and returns the number of characters written.
+"""
 
-def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written.
+
+    Args:
+        filename (str, optional): The name of the file to be written on (default is an empty string).
+        text (str, optional): The text to be written on the file (default is an empty string).
+
+    Returns:
+        int: The number of characters written.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
