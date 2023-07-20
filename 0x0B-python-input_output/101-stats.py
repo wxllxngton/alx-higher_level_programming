72d12a101607f455faf5491d4999895aaa22f71f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-07-20 15:08:14 +0300

    Add files via upload

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,57 @@
 #!/usr/bin/python3
 
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-This is the 'metrics' script.
 
-It reads stdin line by line and computes metrics as described.
-"""
 
-import signal
-import sys
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-def signal_handler(sig, frame):
-    print_metrics()
-    sys.exit(0)
+if __name__ == "__main__":
+    import sys
 
-def print_metrics():
-    total_size = sum([int(line.split()[-1]) for line in lines])
-    print("Total file size:", total_size)
-
+    size = 0
     status_codes = {}
-    for line in lines:
-        status_code = line.split()[8]
-        if status_code in ["200", "301", "400", "401", "403", "404", "405", "500"]:
-            status_codes[status_code] = status_codes.get(status_code, 0) + 1
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-    for status_code in sorted(status_codes):
-        print(status_code + ":", status_codes[status_code])
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-lines = []
-signal.signal(signal.SIGINT, signal_handler)
+            line = line.split()
 
-try:
-    for line in sys.stdin:
-        lines.append(line.strip())
-        if len(lines) == 10:
-            print_metrics()
-            lines = []
-except KeyboardInterrupt:
-    print_metrics()
-    sys.exit(0)
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
