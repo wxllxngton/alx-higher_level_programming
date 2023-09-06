9809a9a9cbc38e7188e7f185e5a149066a8f1a55
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-09-06 10:22:33 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+
+"""
+N-Queens Solver Module
+"""
+
 import sys
 
-
 def is_safe(board, row, col):
-    # Check if a queen can be placed at the given position
-    # without attacking any other queens on the board
+    """
+    Check if a queen can be placed at the given position
+    without attacking any other queens on the board.
+
+    Args:
+        board (list[int]): List representing the current board state.
+        row (int): The current row to check.
+        col (int): The current column to check.
+
+    Returns:
+        bool: True if it's safe to place a queen, False otherwise.
+    """
     for i in range(row):
         if board[i] == col or \
                 board[i] - i == col - row or \
@@ -12,9 +26,18 @@ def is_safe(board, row, col):
             return False
     return True
 
-
 def solve_nqueens(N, board, row):
-    # Recursive function to solve the N-queens problem
+    """
+    Recursive function to solve the N-queens problem.
+
+    Args:
+        N (int): The size of the chessboard (N x N).
+        board (list[int]): List representing the current board state.
+        row (int): The current row being considered.
+
+    Prints:
+        Solutions to the N-queens problem, one solution per line.
+    """
     if row == N:
         # All queens have been placed, print the solution
         print(board)
@@ -23,7 +46,6 @@ def solve_nqueens(N, board, row):
             if is_safe(board, row, col):
                 board[row] = col
                 solve_nqueens(N, board, row + 1)
-
 
 if __name__ == "__main__":
     # Parse command-line arguments
