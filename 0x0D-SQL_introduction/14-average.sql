182df2164fd1262b512efbb09bb473efd519164c
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-10-17 14:47:06 +0300

    Update 14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- Computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
--- cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT AVG(score) AS AVERAGE FROM second_table;
+-- Print the average score
+-- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT AVG(score) AS average FROM second_table;
