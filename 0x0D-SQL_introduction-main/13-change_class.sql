d0cdd4f14ffc8527959819bc6ce2ddc17cce42f3
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-10-17 13:47:03 +0300

    Add files via upload

---
+++ 0x0D-SQL_introduction-main/13-change_class.sql
@@ -0,0 +1,2 @@
+-- Remove all records with a score <= 5 in the table second_table of the specified database.
+DELETE FROM hbtn_0c_0.second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction-main/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Lists all the tables of a database in your MySQL server.
+-- cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
+SHOW TABLES;
