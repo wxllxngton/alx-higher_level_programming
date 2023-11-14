e09dd9a4332d1f9c6cef29e71b6e4c98f72bee0b
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-11-14 09:22:00 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-#!usr/bin/node
-const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+#!/usr/bin/node
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const term of stringArray) {
   console.log(term);
