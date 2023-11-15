183fd7cdd33946d2c4a3e5edd727a984deebeb10
wxllxngton <jjngugi27@gmail.com>
2023-11-14 16:09:12 -0800

    Update: FIles made executable

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,9 +4,9 @@ const convertedArr = Object.entries(dict);
 const newObj = {};
 
 convertedArr.forEach((element) => {
-    newObj[element[1]]
-        ? newObj[element[1]].push(element[0])
-        : (newObj[element[1]] = [element[0]]);
+  newObj[element[1]]
+    ? newObj[element[1]].push(element[0])
+    : (newObj[element[1]] = [element[0]]);
 });
 
 console.log(newObj);
