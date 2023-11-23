3dceab09898e39ce4dd8836014d502688eca8369
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-11-23 17:30:48 +0300

    modified:   101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,56 +3,25 @@
 Script that lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa.
 """
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-
-
-def setup(username, password, database_name):
-    """
-    Set up the SQLAlchemy engine, metadata, and session.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database_name (str): MySQL database name.
-
-    Returns:
-        session: SQLAlchemy session object.
-    """
-    # Create engine and bind it to the session
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            username, password, database_name
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True,
     )
-
-    # Create the metadata
     Base.metadata.create_all(engine)
 
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    return session
-
-
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        exit(1)
-
-    # Call the setup function to create the session
-    session = setup(sys.argv[1], sys.argv[2], sys.argv[3])
-
-    # Query and print all State and City objects
+    session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-
     session.close()
