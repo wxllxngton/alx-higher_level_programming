8848e9f5891a73abf1ceceb91bd32f1a11766d5e
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-11-23 17:40:40 +0300

    modified:   100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,35 +4,62 @@ Script that creates the State "California" with the City "San Francisco"
 from the database hbtn_0e_100_usa.
 """
 
-if __name__ == "__main__":
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
+
+def setup(username, password, database_name):
+    """
+    Set up the SQLAlchemy engine, metadata, and session.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database_name (str): MySQL database name.
+
+    Returns:
+        session: SQLAlchemy session object.
+    """
+    # Create engine and bind it to the session
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, database_name
         ),
         pool_pre_ping=True,
     )
+
+    # Create the metadata
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    # # Create State "California" with City "San Francisco"
-    # california = State(name="California", cities=[City(name="San Francisco")])
-    # session.add(california)
-    # session.commit()
+    return session
 
-    # # Print the new state's ID
-    # print(california.id)
 
-    new_city = City(name="San Francisco")
-    new_states = State(name="California")
-    new.cities.append(new_city)
-    session.add_all([new_states, new_city])
-    session.commit()
-    session.close()
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database_name".format(sys.argv[0]))
+        exit(1)
+
+    # Call the setup function to create the session
+    session = setup(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    # Create State "California" with City "San Francisco"
+    try:
+        california = State(
+            name="California", cities=[City(name="San Francisco")]
+        )
+        session.add(california)
+        session.commit()
+    except Exception as err:
+        session.rollback()
+    finally:
+        # Print the new state's ID
+        print(california.id)
+        session.close()
