934f1c2e333cbadb8e94bc7004403db9dd4157af
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 06:01:13 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Set the variables
+# Script that sets email and subject variables, then sends a POST request using curl to the specified URL
 email="test@gmail.com"
 subject="I%20will%20always%20be%20here%20for%20PLD"
-
-# Send POST request using curl
 curl -s -d "email=$email&subject=$subject" -X POST "$1"
