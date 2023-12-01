ab63d4afea5162ea74976624f2b45d5b93cf4f66
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 05:52:44 +0300

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that sends a request to a URL passed as an argument, 
-#and displays only the status code of the response.
+# Script that sends a request to a URL passed as an argument and displays only the status code of the response.
 curl -L -s -X HEAD -w "%{http_code}" "$1"
