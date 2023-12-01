fb6c747387bb68e44cde46256447c15201121dc0
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 05:51:00 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, 
-#sends a request to that URL,
-#and displays the size of the body of the response
+# Script that takes in a URL sends a request to it and displays the size of the body of the response 
 curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
