b00511edb129f6c9ba515891afc27e682a4ae638
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 05:24:41 +0300

    Create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Script to find a peak in a list of integers for interview preparation.
+"""
+
+"""
+    THOUGHT PROCESS
+        Since the list is not sorted, sorting would take O(n*log(n)).
+            -> Not worth sorting.
+        Looping through and keeping track of the maximum (brute force).
+            -> O(n)
+        Possibly looping from each end and reducing to 1/2 run time.
+            -> Still O(n)
+"""
+
+
+def find_peak(list_of_integers):
+    """Find and return the peak element in the given list of integers.
+
+    Args:
+        list_of_integers (list): A list of integers.
+
+    Returns:
+        int or None: The peak element if found, None if the list is empty.
+    """
+    peak_element = None
+    for element in list_of_integers:
+        if peak_element is None or peak_element < element:
+            peak_element = element
+    return peak_element
