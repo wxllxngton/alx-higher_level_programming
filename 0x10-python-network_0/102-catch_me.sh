b6b2c4cf03893a4dae3f34246988f5fbe118c6a8
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 05:53:18 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Send a POST request to 0.0.0.0:5000/catch_me with the custom header "Origin: ALX"
+# Scipt sends a POST request to 0.0.0.0:5000/catch_me with the custom header "Origin: ALX"
 curl -s -X PUT -H "Origin: ALX" -d "user_id=98" 0.0.0.0:5000/catch_me
