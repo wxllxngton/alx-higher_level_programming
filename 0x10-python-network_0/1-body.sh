a4a2269336edf859bb2e81ff7ca2445f30d4781f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 05:52:18 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, 
-#sends a GET request to the URL, 
-#and displays the body of the response
+# Script that takes in a URL, sends a GET request to it and displays the body of the response
 if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -L
