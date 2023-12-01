1dd001d79aaf61ed08ce778eb776acc699f256f2
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-12-01 04:50:04 +0300

    Create 1-body.sh

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script that takes in a URL, 
+#sends a GET request to the URL, 
+#and displays the body of the response
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -L
