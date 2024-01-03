122c5142efb4d35025e41b872d57e134384c0613
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-01-03 15:06:39 +0300

    Added: New file

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,59 @@
+#!/usr/bin/node
+/**
+ * Script that prints all characters of a Star Wars movie.
+ */
+
+const request = require('request');
+const { exit } = require('process');
+
+if (process.argv.length !== 3) {
+    console.log(`Usage: ./${process.argv[1]} movieId`);
+    exit(1);
+}
+
+let characters = [];
+
+/**
+ * Prints all characters of a Star Wars movie.
+ *
+ * @param {Error} error - The error, if any,
+ * that occurred during the request.
+ * @param {Object} response - The response object containing
+ * information about the request.
+ * @param {string} body - The body of the response.
+ * @returns {void} - The function returns nothing.
+ */
+const printAllCharacters = function (error, response, body) {
+    if (error) {
+        console.error(error);
+        exit(1);
+    }
+
+    const filmData = JSON.parse(body);
+    const characterUrls = filmData.characters;
+
+    // Function to handle each character request
+    const handleCharacterRequest = function (characterUrl) {
+        request(characterUrl, (error, response, body) => {
+            if (error) {
+                console.error(error);
+                exit(1);
+            }
+
+            const data = JSON.parse(body);
+            const characterName = data.name;
+            characters.push(characterName);
+
+            // Print character name after adding to the list
+            console.log(characterName);
+        });
+    };
+
+    // Make requests for each character URL
+    characterUrls.forEach(handleCharacterRequest);
+};
+
+request(
+    `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`,
+    printAllCharacters
+);
