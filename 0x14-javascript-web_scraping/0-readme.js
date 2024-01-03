27986ffcff143906c5dac7362d49a072d6468fde
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-01-03 11:55:26 +0300

    Modified: Fixed some bugs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,17 +6,12 @@ Script that reads and prints the content of a file.
 const fs = require('fs');
 const { exit } = require('process');
 
-// if (process.argv.length == 3) {
-//     console.log(`Usage: ./${proves.argv[1]} file_path`);
-//     exit(1);
-// }
+if (process.argv.length == 3) {
+    console.log(`Usage: ./${proves.argv[1]} file_path`);
+    exit(1);
+}
 
-// fs.readFile(process.argv[2], (err, data) => {
-//     if (err) throw err; // If error occurred while reading the file
-//     console.log(data);
-// });
-
-fs.readFile('./1.json', 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
     if (err) {
         console.error(err); // If error occurred while reading the file
         exit(1);
