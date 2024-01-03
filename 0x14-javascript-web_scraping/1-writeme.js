2766524bbc8e574bf944d9fb72a446d91d2983d0
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-01-03 12:48:27 +0300

    Added: 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/**
+ * Script that writes a string to a file.
+ */
+
+const fs = require('fs');
+const { exit } = require('process');
+
+if (process.argv.length != 4) {
+    console.log(`Usage: ./${process.argv[1]} file_path string_to_write`);
+    exit(1);
+}
+
+fs.writeFile(
+    process.argv[2],
+    process.argv[3],
+    {
+        encoding: 'utf8',
+        flag: 'w',
+        mode: 0o666,
+    },
+    (error) => {
+        if (error) {
+            console.error(error);
+            exit(1);
+        }
+    }
+);
